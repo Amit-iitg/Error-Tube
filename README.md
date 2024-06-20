9d3b2654a2ea83c726474965fceb02295b536e55
AMIT RATHORE <rathoream999@gmail.com>
2024-06-20 14:54:04 +0530

    first Phase

---
+++ README.md
@@ -0,0 +1,20 @@
+# ErrorTube
+
+## Project Description
+
+
+We made a video streaming platform for syntax error
+your team is Error- 404
+
+## Installation
+
+To run the code
+1. You need to download nodejs form `https://nodejs.org/en/download`
+2. Then open( `project-directory`\01proj )folder in cmd or vscode
+3. you also have to download videos from `https://drive.google.com/drive/folders/1LnHTzyDYcgk4hzTdq4p76I17NvPHY78r?usp=sharing`
+and paste it at `proj-direc`/public/videos.  
+4. run command `npm i`
+5. them run command `npm run dev`
+6. search button also works ,you have to click on search instead to pressing enter
+7. on recommendation section below player after clicking the video please reload 
+
---
+++ src/components/contact.jsx
@@ -0,0 +1,41 @@
+import { Margin } from '@mui/icons-material';
+import React from 'react'
+import { ReactDOM } from 'react-dom';
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <hr></hr>
+        <h1>About us</h1>
+        <hr></hr>
+
+        <ul>
+        <h5 style={{marginRight:"30px"}}>
+          Welcome to MovieTube, your ultimate destination for all things cinema! Whether you are a casual moviegoer, a dedicated cinephile, or someone who simply loves to explore new films, MovieTube is designed to cater to your every cinematic need.
+        </h5>
+
+            
+            
+            
+        </ul>
+        <hr/>
+        <h2>Contact us</h2>
+        <h5 style={{marginRight:"30px"}}>
+          rathoream999@gmail.com
+          
+        </h5>
+        <h5 style={{marginRight:"30px"}}>
+          Phone no. 7827532622
+          
+        </h5>
+        <hr/>
+        <ul>
+            
+        </ul>
+
+        
+    </div>
+  )
+}
+
+export default Contact
